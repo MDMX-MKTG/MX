7ba7d5a3194c662864c61730740dbdc87f8b1268
MDMX-MKTG <oficialmbmd@gmail.com>
2025-05-16 22:14:55 -0300

    Update d-mp4.js

--- plugins/d-mp4.js
+++ plugins/d-mp4.js
@@ -1,3 +1,51 @@
+import fetch from 'node-fetch'
+import yts from 'yt-search'
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+if (!text) {
+return conn.sendMessage(m.chat, { text: `*[ ? ]*Ingrese el comando mas el nombre de la cancion que busca.\n\n• *Por ejemplo:*\n${usedPrefix + command} Ethereal` }, { quoted: m })
+}
+let ytres = await search(args.join(" "))
+if (!ytres.length) {
+return conn.sendMessage(m.chat, { text: `*[ ✘ ]*  No se han encontrado resultados de la busqueda.\n- Intentalo de nuevo con otro nombre.`}, { quoted: m })
+}
+
+await conn.sendMessage(m.chat, { text: `ⴵ _Descargando el pedido, espere un momento..._` }, { quoted: m })
+let mdmx = ytres[0]
+let txt = `•─• •⟤ \`MP4 • YOUTUBE\` ⟥• •─•
+
+⊸⊹ *Titulo:* ${mdmx.title}
+⊸⊹ *Duracion:* ${mdmx.timestamp}
+⊸⊹ *Publicado:* ${mdmx.ago}
+⊸⊹ *Autor/a:* ${mdmx.author.name || 'Desconocido'}
+⊸⊹ *Enlace:* ${mdmx.url}`
+await conn.sendFile(m.chat, mdmx.image, 'thumbnail.jpg', txt, m)
+
+try {
+const resolution = '360'
+const response = await fetch(`https://cloudkutube.eu/api/ytv?url=${encodeURIComponent(izumi.url)}&resolution=${resolution}`)
+const data = await response.json()
+
+if (data.status !== 'success') throw new Error('Fallo al obtener el vídeo.')
+
+const title = data.result.title
+const videoUrl = data.result.url
+
+await conn.sendMessage(m.chat, { video: { url: videoUrl }, mimetype: "video/mp4", caption: txt }, { quoted: m })
+} catch (error) {
+console.error(error)
+conn.sendMessage(m.chat, { text: `⦗ ✘ ⦘ _Ocurrio un error con el comando: *${usedPrefix + command}*_\n- _Reporta el error al grupo de asistencia o usa el comando: *${usedPrefix}report*_` }, { quoted: m })
+}
+}
+
+handler.command = ["mp4", "video"]
+export default handler
+
+async function search(query, options = {}) {
+let search = await yts.search({ query, hl: "es", gl: "ES", ...options })
+return search.videos
+}
+
+/*
 import fetch from "node-fetch";
 import axios from 'axios';
 
@@ -34,7 +82,7 @@ export default handler;
 
 async function ytdl(url) {
 const headers = {
-"accept": "*/*",
+"accept": "**",
 "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
 "sec-ch-ua": "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\"",
 "sec-ch-ua-mobile": "?1",
@@ -93,4 +141,4 @@ return contentLength ? parseInt(contentLength, 10) : null;
 return error;
 }
 }
-
+*/
