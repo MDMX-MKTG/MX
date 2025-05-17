f01d51ad2273ac949b250d2b044a908deb4961bb
MDMX-MKTG <oficialmbmd@gmail.com>
2025-05-16 22:34:00 -0300

    Update i-info.js

--- plugins/i-info.js
+++ plugins/i-info.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, command, usedPrefix }) => {
+let handler = async (m, { conn, command, usedPrefix, args }) => {
 let user = db.data.users[m.sender]
 if (!args[0]) {
 let informacion = `
